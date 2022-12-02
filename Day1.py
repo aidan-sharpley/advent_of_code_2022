b37cf2ce09e4acaa83800c41df28611abdca0bc4
Aidan <52019584+aidan-sharpley@users.noreply.github.com>
2022-12-02 13:13:05 -0500

    l33tcode feels

--- Day1.py
+++ Day1.py
@@ -55,8 +55,6 @@
 # then the fifth Elf (with 10000 Calories). The sum of the Calories carried by these three elves is 45000.
 
 # Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
-import html
-
 inputFile = 'Day1.dat'
 
 def partOne(f):
